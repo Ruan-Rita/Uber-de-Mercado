3a8d3afb438b2a90ef58ee14da477a894cfbe5a9
RUAN RITA <ruan.rita@fatec.sp.gov.br>
2020-08-30 18:09:21 -0300

    Menu e Rodape SemiProntos

--- UberComida/home.php
+++ UberComida/home.php
@@ -10,7 +10,6 @@
 
 </head>
 <body>
-<canvas></canvas>
     <div class="main">
         <div class="intro">
             <img src="img/Candy_Many_Texture_519849_2560x1440.jpg" width="100%" height="100%"/>
@@ -39,17 +38,45 @@
 
 
     </div>
+
     <div class="footer">
-        <div class="marca">
-            <h2>INRITA</h1>
-        </div>
-        <div class="redeSociais">
-            <h1>Rede Social</h1>
-            <div class="redes"></div>
-        </div>
-        <div class="extras">
-            <h1>EMPRESA</h1>
-            <p>Quem somos</p>
+        <div class="cont">
+            <div class="marca">
+                <h2>MARCA</h1>
+            </div>
+            <div class="redeSociais">
+                <h1>Rede Social</h1>
+                <div class="redes">
+                    <div class="rede1"><img id="img" src="img/facebook.png" width="100%" height="100%"/></div>
+                    <div class="rede2"><img id="img" src="img/youtube.png" width="100%" height="100%"/></div>
+                    <div class="rede3"><img id="img" src="img/instagram.png" width="100%" height="100%"/></div>
+                    <div class="rede4"><img id="img" src="img/linkedin.png"  width="100%" height="100%"/></div>
+                </div>
+            </div>
+            <div class="extras">
+                <div  class="part1">
+                    <h1>Empresa</h1>
+                    <p>Quem somos</p>
+                    <p>Empregos</p>
+                    <p>Blog</p>
+                </div>
+                <div class="part2">
+                    <h1>Produtos</h1>
+                    <p>Alimenticio Eletronico Limpeza</p>
+                    <p>Faça Entregas</p>
+                    <p>Digitalize sua Empresa</p>
+                </div>
+                <div class="part3">
+                    <h1>Links Úteis</h1>
+                    <p>Ajuda</p>
+                    <p>Faça Downlaod do App</p>
+                    <p>Como ser um colaborador</p>
+                </div>
+            </div>
+
+            <div class="autoral">
+                <h1>@ Copyright INRITA Technologies Inc.</h1>
+            </div>
         </div>
     </div>    
 </body>
